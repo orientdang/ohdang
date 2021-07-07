6894c47da14f68a2110408dbe3fdfc08724df470
dang-codelink <dang@codelink.io>
2021-07-07 22:02:19 +0700

    add first blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
         'gatsby-plugin-image',
         'gatsby-plugin-react-helmet',
         'gatsby-plugin-sitemap',
+        'gatsby-transformer-remark',
         {
             resolve: 'gatsby-plugin-manifest',
             options: {
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,16 +1,24 @@
 import * as React from 'react'
+import { graphql, Link } from 'gatsby'
 import Layout from '../components/layout'
-import { graphql } from 'gatsby'
 
 const BlogPage = ({ data }) => {
+    const renderBlogs = () => {
+        const filtered_data = data.allFile.nodes.filter(
+            (node) => node.ext === '.md'
+        )
+        return filtered_data.map((node) => {
+            return (
+                <li key={node}>
+                    <Link to={`/${node.name}`}>{node.name}</Link>
+                </li>
+            )
+        })
+    }
+
     return (
         <Layout pageTitle="My Blog Posts">
-            <p>My cool posts will go in here</p>
-            <ul>
-                {data.allFile.nodes.map((node) => (
-                    <li key={node.name}>{node.name}</li>
-                ))}
-            </ul>
+            <ul>{renderBlogs()}</ul>
         </Layout>
     )
 }
@@ -20,8 +28,10 @@ export const query = graphql`
         allFile {
             nodes {
                 name
+                ext
             }
         }
     }
 `
+
 export default BlogPage
